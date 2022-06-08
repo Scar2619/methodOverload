73f16453dcffc5646568c91cf811aadae9286d26
Katelind Phillips <scarlettkate94@gmail.com>
2022-06-08 17:40:34 -0400

    final

--- methodOverload/Program.cs
+++ methodOverload/Program.cs
@@ -6,7 +6,36 @@ namespace methodOverload
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+        public static int Add(int x, int y)
+        {
+            return x += y;
+        }
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x += y;
+        }
+        public string Add(int x, int y, bool isTrue)
+        {
+            var sum = 0;
+            var response = "";
+
+            if (isTrue)
+            {
+                sum = x + y;
+
+                if (sum == 1)
+                {
+                    response = $"{sum} dollar.";
+                }
+                else
+                {
+                    response = $"{sum} dollars.";
+                }
+            }
+            return response;
+
         }
     }
 }
